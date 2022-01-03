e182756ec5d5c03effe28f6ed4f76335442dbe4a
mhk98 <mohsinkabirseo@gmail.com>
2022-01-03 14:41:12 +0600

    hunt commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+
+
+  $('#banner').slick({
+    dots: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    prevArrow:'<i class="fa fa-arrow-left icon left"></i>',
+   nextArrow:'<i class="fa fa-arrow-right icon right"></i>',
+  });
+
+})
